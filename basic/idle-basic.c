6c8ca4310f1b56db48a1230c3ce7c5063ee987fb
Keita Akutsu <kakts.git@gmail.com>
2019-01-29 23:22:30 +0900

    Add idle-basic.c

---
+++ basic/idle-basic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <uv.h>
+
+int64_t counter = 0;
+
+void wait_for_a_while(uv_idle_t* handle) {
+  counter++;
+  // カウントが一定数たったら停止
+  if (counter >= 10e6) {
+    uv_idle_stop(handle);
+  }
+}
+
+int main() {
+  uv_idle_t idler;
+
+  // idle watcherの初期化
+  uv_idle_init(uv_default_loop(), &idler);
+  uv_idle_start(&idler, wait_for_a_while);
+
+  printf("Idling... \n");
+
+  // ループを動かす
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+
+  uv_loop_close(uv_default_loop());
+  return 0;
+}
